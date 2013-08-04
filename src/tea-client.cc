60cce92897b5e6fcbeb2461f1d7399554f303e96
Cahu <cahu@gmx.us>
2013-08-04 12:52:58 +0200

    moving net init in its own function

--- src/tea-client.cc
+++ src/tea-client.cc
@@ -6,25 +6,51 @@
 #include <sys/socket.h>
 
 #define PORT 9999
+#define MAX_MSG_LEN 64
+
+
+static int tcp_sock;
+static int udp_sock;
+
+void init_connect(const char *, unsigned short);
 
 
 int main(int argc, char *argv[])
 {
+	if (argc < 2) {
+		init_connect("127.0.0.1", PORT);
+	} else {
+		init_connect(argv[1], PORT);
+	}
+
+	close(tcp_sock);
+	close(udp_sock);
+
+	exit(EXIT_SUCCESS);
+}
+
+
+
+void init_connect(const char *addr, unsigned short port)
+{
 	int rv;
-	int tcp_sock, udp_sock;
 	sockaddr_in server_addr;
 
 	server_addr.sin_family = AF_INET;
-	server_addr.sin_port   = htons(PORT);
-	if (argc < 2) {
-		inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr.s_addr);
-	} else {
-		inet_pton(AF_INET, argv[1], &server_addr.sin_addr.s_addr);
-	}
+	server_addr.sin_port   = htons(port);
+	inet_pton(AF_INET, addr, &server_addr.sin_addr.s_addr);
 
 	udp_sock = socket(AF_INET, SOCK_DGRAM, 0);
 	if (udp_sock < 0) {
 		perror("socket");
+		exit(EXIT_FAILURE);
+	}
+
+	// yes, connect an udp socket.
+	rv = connect(udp_sock, (sockaddr *) &server_addr, sizeof server_addr);
+	if (rv < 0) {
+		perror("connect");
+		close(udp_sock);
 		exit(EXIT_FAILURE);
 	}
 
@@ -40,9 +66,4 @@ int main(int argc, char *argv[])
 		close(tcp_sock);
 		exit(EXIT_FAILURE);
 	}
-
-	close(tcp_sock);
-	close(udp_sock);
-
-	exit(EXIT_SUCCESS);
 }
