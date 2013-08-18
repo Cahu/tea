35ba924ff1d33b6cd2e6fe055f9b5cf61ce6c360
Cahu <cahu@gmx.us>
2013-08-19 00:01:14 +0200

    graphics.hh fixed

--- inc/graphics.hh
+++ inc/graphics.hh
@@ -1,9 +1,14 @@
 #ifndef _GRAPHICS_HH_
 #define _GRAPHICS_HH_
 
+#include <vector>
+
+#include "Player.hh"
+
+
 void init_sdl();
 void init_opengl();
 void init_world(const char *);
-void draw_scene(const std::vector<Player *> &, float, float);
+void draw_scene(const std::vector<TEA::Player *> &, float, float);
 
 #endif
