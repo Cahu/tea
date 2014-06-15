297a2a4862e2eaf8e0fc08a616e0093773bae6db
cahu <cahu@gmx.us>
2014-06-15 20:28:30 +0200

    quiet GLM messages

--- src/graphics.cc
+++ src/graphics.cc
@@ -1,7 +1,8 @@
 #include <GL/glew.h>
 #include <SDL/SDL.h>
 
-#include <glm/glm.hpp> // math lib
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
